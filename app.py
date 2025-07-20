6b66938ce6323d30136b5385a005350a092936f3
diglander <denisovchinn2094@gmail.com>
2025-07-20 07:51:46 +0300

    Добавлена логика классов

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, url_for #для гибких гиперссылок в HTML-странице
+from flask import Flask, url_for, g #для гибких гиперссылок, хуки
 import requests
 from dotenv import load_dotenv # для чтения api-key′я из .env
 import os # для записи api-key′я из .env
+from api_clients import ExchangeAPI
 
 load_dotenv() #подгрузка .env в область видимости (scope)
 
@@ -9,42 +10,35 @@ app = Flask(__name__)
 
 API_KEY = os.getenv('EXCHANGE_API_KEY')
 
-full_url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/USD"
+Exchange_Client = ExchangeAPI(API_KEY)
 
-def get_rates_from_exchange():
-    try:
-        response = requests.get(full_url)
-        response.raise_for_status() #для правильной работы «try»
-        data = response.json()
-        
-        if data.get('result') == 'success':
-            return data.get('conversion_rates')
-    except:
-        print("ОШИБКА!!!")
-        return None #чтобы сделать проверку на корректность в декораторах 
-
+@app.before_request
+def load_rates():
+    Exchange_Client.fetch_rates()
+    if Exchange_Client.success:
+        g.rates = Exchange_Client.rates
+    else:
+        g.rates = None
 
 @app.route('/')
 def intro():
     return f"""
 <h1>Привет! Выбирай что хочешь!</h1> 
-<p><a href="{url_for('USD_RUB')}"> Рубль</p>
-<p><a href="{url_for('USD_CNY')}"> Юань</p>
+<p><a href="{url_for('USD_RUB')}"> Рубль</a></p>
+<p><a href="{url_for('USD_CNY')}"> Юань</a></p>
 """ #использовали url_for для гиперссылок
 
 @app.route('/RUB')
 def USD_RUB():
-    rates = get_rates_from_exchange()
-    if rates:
-        return f'<h1><a href="{url_for('intro')}"> В 1 долларе {rates['RUB']} рублей</h1>'
+    if (g.rates and g.rates.get('RUB')):
+        return f'<h1><a href="{url_for('intro')}"> В 1 долларе {g.rates.get('RUB')} рублей</h1>'
     else:
         return "<h1>Ошибка</h1>"
 
 @app.route('/CNY')
 def USD_CNY():
-    rates = get_rates_from_exchange()
-    if rates:
-        return f'<h1><a href="{url_for('intro')}"> В 1 долларе {rates['CNY']} юаней</h1>'
+    if (g.rates and g.rates.get('CNY')):
+        return f'<h1><a href="{url_for('intro')}"> В 1 долларе {g.rates.get('CNY')} юаней</h1>'
     else:
         return "<h1>Ошибка</h1>"
 
